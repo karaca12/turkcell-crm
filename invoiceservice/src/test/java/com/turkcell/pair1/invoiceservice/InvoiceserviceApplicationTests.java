25f1d17f4ddfb818f454a6a5662a2914eb6b9b39
karaca <mekaraca18@gmail.com>
2024-03-16 15:34:32 +0300

    Initial commit

---
+++ invoiceservice/src/test/java/com/turkcell/pair1/invoiceservice/InvoiceserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.pair1.invoiceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ productservice/src/test/java/com/turkcell/pair1/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.pair1.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
