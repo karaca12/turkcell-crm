4ad09ab53269a1c94a7d3530bfbf9297a5309d79
karaca <mekaraca18@gmail.com>
2024-04-19 19:29:29 +0300

    fix core package implementations

--- authservice/src/main/java/com/turkcell/pair1/authservice/AuthserviceApplication.java
+++ authservice/src/main/java/com/turkcell/pair1/authservice/AuthserviceApplication.java
@@ -1,19 +1,15 @@
 package com.turkcell.pair1.authservice;
 
+import com.turkcell.pair1.annotation.EnableSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.turkcell.pair1"})
-@EntityScan(basePackages = {"com.turkcell.pair1.entity"})
-@EnableJpaRepositories(basePackages = {"com.turkcell.pair1.repository"})
+@EnableSecurity
 public class AuthserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthserviceApplication.class, args);
+    }
 
 }
