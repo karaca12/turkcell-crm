34b1ac1ffc6bce75e1663011843ff1010c023d92
karaca <mekaraca18@gmail.com>
2024-05-01 22:41:56 +0300

    fix

--- productservice/src/main/java/com/turkcell/pair1/productservice/controller/CatalogueController.java
+++ productservice/src/main/java/com/turkcell/pair1/productservice/controller/CatalogueController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/catalogues")
+@RequestMapping("/api/products/catalogues")
 @RequiredArgsConstructor
 public class CatalogueController {
     private final CatalogueService catalogueService;
