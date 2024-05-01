e794999c93c9150b2b7989cac49bab206aa55c32
karaca <mekaraca18@gmail.com>
2024-05-01 19:37:07 +0300

    1.3.3 common implementation

--- customerservice/src/main/java/com/turkcell/pair1/customerservice/client/OrderServiceClient.java
+++ customerservice/src/main/java/com/turkcell/pair1/customerservice/client/OrderServiceClient.java
@@ -1,12 +1,17 @@
 package com.turkcell.pair1.customerservice.client;
 
 import com.turkcell.pair1.configuration.feign.FeignClientConfiguration;
+import com.turkcell.pair1.customerservice.service.dto.response.CustomerHasActiveProductsResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "orderservice", configuration = FeignClientConfiguration.class)
 public interface OrderServiceClient {
     @GetMapping("/api/orders/getCustomerIdByOrderId")
     String getCustomerIdByOrderId(@RequestParam("orderId") String orderId);
+
+    @GetMapping("/api/orders/customer/{customerId}/hasActiveProducts")
+    CustomerHasActiveProductsResponse customerHasActiveProducts(@PathVariable String customerId);
 }
---
+++ customerservice/src/main/java/com/turkcell/pair1/customerservice/service/dto/response/CustomerHasActiveProductsResponse.java
@@ -0,0 +1,8 @@
+package com.turkcell.pair1.customerservice.service.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class CustomerHasActiveProductsResponse {
+    private boolean hasActiveProducts;
+}
